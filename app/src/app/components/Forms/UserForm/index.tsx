17db3b5670ed42588e30961c88ed2efe0d46e0b8
gabrielcardn <gabrielcard2001@gmail.com>
2024-02-25 15:07:47 -0300

    bullding user form

--- app/src/app/components/Forms/UserForm/index.tsx
+++ app/src/app/components/Forms/UserForm/index.tsx
@@ -2,5 +2,26 @@ import Link from "next/link";
 import styles from "./UserForm.module.css";
 
 export default function UserForm() {
-  return <button>UserForm</button>;
+  return (
+    <div>
+      <form>
+        <label>Código</label>
+        <input
+          type="text"
+          id="code"
+          name="Código"
+          placeholder="Código do usuário"
+        />
+        <label>Nome</label>
+        <input
+          type="text"
+          id="name"
+          name="Nome"
+          placeholder="Nome do usuário"
+        />
+        <label>CPF</label>
+        <input type="number" id="cpf" name="CPF" placeholder="CPF do usuário" />
+      </form>
+    </div>
+  );
 }
